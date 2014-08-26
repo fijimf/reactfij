be9f3abbbeb2cbd5b0847a36033ba0b5bae11626
fijimf <fijimf@gmail.com>
2014-08-26 10:40:28 -0400

    Create NcaaSeasonUpdater.scala
    Initial cut may not compile

---
+++ app/model/scraping/actors/NcaaSeasonUpdater.scala
@@ -0,0 +1,15 @@
+package model.scraping.actors
+
+case class TeamMap(keys:Set[String], data:Map[String,Any])
+
+
+class NcaaSeasonUpdater extends Actor {
+
+  def receive {
+      
+  
+  
+  }
+  private def loadTeams = {
+     
+}
