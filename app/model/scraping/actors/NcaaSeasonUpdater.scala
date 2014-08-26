f2c353a324ae19c62ea57b887c7ccc057511e66e
fijimf <fijimf@gmail.com>
2014-08-26 10:50:30 -0400

    Update NcaaSeasonUpdater.scala

--- app/model/scraping/actors/NcaaSeasonUpdater.scala
+++ app/model/scraping/actors/NcaaSeasonUpdater.scala
@@ -1,15 +1,23 @@
 package model.scraping.actors
 
-case class TeamMap(keys:Set[String], data:Map[String,Any])
+case class TeamMap(fieldKeys:Set[String], data:Map[String,Map[String,Any]]) {
+  def update
+}
 
 
+case class UpdateSeason(start:Localdate, end:LocalDate, teamData:Map[String,Map[String,Any]])
 class NcaaSeasonUpdater extends Actor {
 
   def receive {
+    case UpdateSeason(start, end, teamData) => 
       
   
-  
+    case 
   }
-  private def loadTeams = {
-     
+  
+  // vv Move to another Object vv
+  // private def loadTeams = {
+  //   TeamListLoader.loadTeamList((s:Seq[TeamLink]])=>{
+  //     s.filter(_.name.isDefined).map(t=>(t.url->Map("name"->t.name, "key"->t.url))).toMap
+  //   }).
 }
