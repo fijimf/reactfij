8b306716f8278ebbf1c0fb716552ee2c42cd7478
fijimf <fijimf@gmail.com>
2014-08-26 23:23:42 -0400

    Some more work

--- app/model/scraping/actors/NcaaSeasonUpdater.scala
+++ app/model/scraping/actors/NcaaSeasonUpdater.scala
@@ -1,19 +1,48 @@
 package model.scraping.actors
 
+import akka.actor.{Props, Actor}
+import model.scraping.model.TeamLink
+import model.scraping.scrapers.TeamListScraper
+import org.joda.time.LocalDate
+import play.api.data.validation.ValidationError
+import play.api.libs.json.JsPath
+
+import scala.concurrent.Future
+
 case class TeamMap(fieldKeys:Set[String], data:Map[String,Map[String,Any]]) {
-  def update
+  def update(data:Map[String, Object]): Unit = {
+
+  }
 }
 
 
-case class UpdateSeason(start:Localdate, end:LocalDate, teamData:Map[String,Map[String,Any]])
+case class UpdateSeason(start:LocalDate, end:LocalDate, teamData:Map[String,Map[String,Any]])
 class NcaaSeasonUpdater extends Actor {
+  val sbd = context.actorOf[Props[DailyScoreboardActor]]
+  def receive:Receive = {
+    case UpdateSeason(start, end, teamData) =>
+      val kernelFut: Future[Either[Seq[(JsPath, Seq[ValidationError])], Map[String, Map[String, Object]]]] = TeamListScraper.loadTeamList((links: Seq[TeamLink]) => {
+        links.foldLeft(Map.empty[String, Map[String, Object]])((d: Map[String, Map[String, Object]], link: TeamLink) => {
+          link.name match {
+            case Some(n) => d + (link.url -> Map("key" -> link.url, "name" -> n))
+            case None => d
+          }
+        })
+      })
+      kernelFut.map((e: Either[Seq[(JsPath, Seq[ValidationError])], Map[String, Map[String, Object]]]) =>
+        e match {
+          case Left(_) =>
+          case Right(teamKernel) => {
+            Iterator.iterate(start)(_.plusDays(1)).takeWhile(_.isBefore(end)).foreach(date=>{
 
-  def receive {
-    case UpdateSeason(start, end, teamData) => 
-      
-  
-    case 
+            })
+          }
+        }
+                   )
+
   }
+
+
   
   // vv Move to another Object vv
   // private def loadTeams = {
